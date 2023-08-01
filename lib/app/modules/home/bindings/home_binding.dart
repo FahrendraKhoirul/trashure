c37e7562a08cc056f19ca85c15472d1798554475
Fahrendra Khoirul <fahrendra.khoirul@gmail.com>
2023-08-01 12:18:16 +0700

    add constant

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,10 +1,19 @@
 import 'package:get/get.dart';
 
+import 'package:trashure/app/modules/home/controllers/navbar_controller.dart';
+import 'package:trashure/app/modules/home/controllers/profile_controller.dart';
+
 import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<ProfileController>(
+      () => ProfileController(),
+    );
+    Get.lazyPut<NavbarController>(
+      () => NavbarController(),
+    );
     Get.lazyPut<HomeController>(
       () => HomeController(),
     );
